1cb344cb5ccfe6135ef2b453265d8177acafc5d5
ChVictoria <butterflylight01@gmail.com>
2023-10-17 21:15:55 +0300

    add rounding quantities of ingredients

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,27 @@
-let portions = 8
-let newPortions = 8
-function productQuantity(currentQuantity){
-    let quantityPerPortion = currentQuantity/portions
-    return quantityPerPortion*newPortions
+let portions = null
+let quantities = null
 
-}
 
 function countQuantity(){
-    debugger;
-    let quantities = document.getElementsByClassName("quantity")
-    newPortions = Number(document.getElementById("portions-number").value)
+
+    let newPortions = Number(document.getElementById("portions-number").value)
     if (newPortions <= 0){
         return
     }
-    for (let i = 0; i < quantities.length; i++){
-        quantities[i].innerHTML = productQuantity(Number(quantities[i].textContent))
+
+    if (quantities == null){
+        quantities = []
+        portions = Number(document.getElementById("portions-number").getAttribute("placeholder"))
+        let initQuantities = document.getElementsByClassName("quantity")
+        for (let i = 0; i < initQuantities.length; i++) {
+            quantities.push(Number(initQuantities[i].textContent) / portions)
+        }
     }
-    portions = newPortions
+
+    let newQuantities = document.getElementsByClassName("quantity")
+    for (let i = 0; i < quantities.length; i++){
+        newQuantities[i].innerHTML = Math.round(quantities[i]*newPortions*100)/100
+    }
+
 }
 
