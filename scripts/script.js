26950b604624c176a47c27b88eb9cd1d8e34e6fc
ChVictoria <butterflylight01@gmail.com>
2023-10-18 21:06:31 +0300

    add eventlistener instead of oninput attr

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
-let portions = null
-let quantities = null
-
+let quantities = []
+let portions = Number(document.getElementById("portions-number").getAttribute("placeholder"))
+let initQuantities = document.getElementsByClassName("quantity")
+for (let i = 0; i < initQuantities.length; i++) {
+    quantities.push(Number(initQuantities[i].textContent) / portions)
+}
 
 function countQuantity(){
 
@@ -9,19 +12,12 @@ function countQuantity(){
         return
     }
 
-    if (quantities == null){
-        quantities = []
-        portions = Number(document.getElementById("portions-number").getAttribute("placeholder"))
-        let initQuantities = document.getElementsByClassName("quantity")
-        for (let i = 0; i < initQuantities.length; i++) {
-            quantities.push(Number(initQuantities[i].textContent) / portions)
-        }
-    }
-
     let newQuantities = document.getElementsByClassName("quantity")
     for (let i = 0; i < quantities.length; i++){
-        newQuantities[i].innerHTML = Math.round(quantities[i]*newPortions*100)/100
+        newQuantities[i].textContent = Math.round(quantities[i]*newPortions*100)/100
     }
 
 }
+
+document.getElementById("portions-number").addEventListener("input", countQuantity)
 
